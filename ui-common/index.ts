6ad9f7a883fb95114e86fee7e312d800f5a7f2b8
Pablo Fernandez <p@f7z.io>
2023-08-30 13:36:59 +0300

    ElementConnector

--- ui-common/index.ts
+++ ui-common/index.ts
@@ -1,7 +1,24 @@
 import './styles.css';
+
+/**
+ * Buttons
+ */
+export { default as AttentionButton } from './src/components/buttons/AttentionButton.svelte'
+
+/**
+ * Generic Components
+ */
 export { default as PageTitle } from './src/components/PageTitle.svelte'
 export { default as MenuItem } from './src/components/Sidebar/MenuItem.svelte'
 export { default as PopularShelves } from './src/components/lists/PopularShelves.svelte'
+export { default as ElementConnector } from './src/components/ElementConnector.svelte'
+
+/**
+ * Layouts
+ */
 export { default as MainWithRightSidebar } from './src/layouts/MainWithRightSidebar.svelte'
 
-export { default as AttentionButton } from './src/components/buttons/AttentionButton.svelte'
+/**
+ * Modals
+ */
+export { default as ModalWrapper } from './src/components/modals/ModalWrapper.svelte'
---
+++ ui-common/src/components/ElementConnector.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+    export let from: HTMLElement;
+
+    let bottomOfFrom: number = 0;
+    let container: HTMLElement | undefined = undefined;
+
+    let topOfContainer: number = 0;
+
+    $: if (from) {
+		bottomOfFrom = from.getBoundingClientRect().bottom;
+	}
+
+	$: if (container) {
+		topOfContainer = container.getBoundingClientRect().top;
+	}
+
+    // when the window is resized, recalculate the positions
+    window.addEventListener('resize', () => {
+        if (from) {
+            bottomOfFrom = from.getBoundingClientRect().bottom;
+        }
+
+        if (container) {
+            topOfContainer = container.getBoundingClientRect().top;
+        }
+    });
+
+	setInterval(() => {
+		if (from) {
+			bottomOfFrom = from.getBoundingClientRect().bottom;
+		}
+
+		if (container) {
+			topOfContainer = container.getBoundingClientRect().top;
+		}
+	}, 2000);
+</script>
+
+<div
+    class={$$props.class}
+    bind:this={container}
+>
+    <div class="
+        absolute
+        connector
+    " style="
+        margin-left: -20px;
+        width: 20px;
+        border-bottom-left-radius: 1rem;
+        height: {topOfContainer - bottomOfFrom + 20}px;
+        margin-top: -{topOfContainer - bottomOfFrom}px;
+    "></div>
+    <slot />
+</div>
