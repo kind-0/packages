f46bb04115333b70399044a62e3dd555023a0d2b
Pablo Fernandez <p@f7z.io>
2023-09-14 15:22:51 +0300

    Improve attention button

--- ui-common/src/lib/components/buttons/AttentionButton.svelte
+++ ui-common/src/lib/components/buttons/AttentionButton.svelte
@@ -1,20 +1,31 @@
 <script lang="ts">
     export let href: string | undefined = undefined;
+    export let color: 'accent' | 'accent2' = 'accent2'
 </script>
 
 {#if href}
-    <a {href} on:click class={$$props.class??""}>
+    <a {href} on:click class="{color} {$$props.class??""}">
         <slot />
     </a>
 {:else}
-    <button on:click class={$$props.class??""}>
+    <button on:click class="{color} {$$props.class??""}">
         <slot />
     </button>
 {/if}
 
 <style lang="postcss">
     a, button {
-        @apply btn h-11 bg-base-100 rounded-full border-2 border-accent2 text-base-100-content font-medium normal-case hover:bg-accent2 hover:bg-opacity-20 hover:border-accent2 overflow-hidden whitespace-nowrap flex-nowrap;
+        @apply btn h-11 bg-base-100 rounded-full border-2 text-base-100-content font-medium normal-case   overflow-hidden whitespace-nowrap flex-nowrap;
+        @apply transition-all duration-100;
+    }
+
+    a.accent, button.accent {
+        @apply border-accent hover:bg-accent/20 hover:border-accent;
+        box-shadow: 0px 0px 21px 2px #FB603855;
+    }
+
+    a.accent2, button.accent2 {
+        @apply border-accent2 hover:bg-accent2/20 hover:border-accent2;
         box-shadow: 0px 0px 21px 2px rgba(142, 113, 255, 0.33);
     }
 </style>
