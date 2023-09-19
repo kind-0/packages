d0519b145688f6c446caa0f77c28e5f34402cd75
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2023-09-18 23:50:19 -0600

    Update MainWithRightSidebar

--- ui-common/src/lib/layouts/MainWithRightSidebar.svelte
+++ ui-common/src/lib/layouts/MainWithRightSidebar.svelte
@@ -1,12 +1,8 @@
-<script lang="ts">
-</script>
-
-<div class="flex flex-col-reverse lg:flex-row gap-9">
-    <main class="flex-grow">
+<div class="flex flex-row gap-32 flex-grow">
+    <main class="w-main flex flex-col justify-stretch">
         <slot />
     </main>
-
-    <aside class="sticky top-8 hidden shrink-0 md:block w-72">
-        <slot name="right-sidebar" />
-    </aside>
+    <asaide class="w-sidebar sticky top-0 max-h-[90vh] overflow-y-auto">
+        <slot name="rightSidebar" />
+    </asaide>
 </div>
