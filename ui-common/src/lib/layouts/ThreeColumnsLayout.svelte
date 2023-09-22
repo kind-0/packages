f338ce6747fb6af02803ce9775d42a08bbe40cfe
triesap <137732411+triesap@users.noreply.github.com>
2023-09-22 18:24:11 +0000

    update ThreeColumnsLayout

--- ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
+++ ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
@@ -2,14 +2,14 @@
     import Toaster from "../components/Toaster/Toaster.svelte";
 </script>
 
-<div class="w-fit mx-auto flex flex-col gap-8 min-h-screen">
+<div class="w-screen lg:w-fit mx-auto flex flex-col gap-8 min-h-screen">
     {#if $$slots.navbar}
         <div class="flex flex-row gap-32 sticky top-0 z-50">
             <slot name="navbar" />
         </div>
     {/if}
 
-    <div class="flex flex-row">
+    <div class="flex flex-row px-4 gap-4">
         <div class="hidden lg:block w-auto lg:w-sidebar sticky max-h-[90vh] overflow-y-auto top-0">
             {#if $$slots.sidebar}
                 <slot name="sidebar" />
