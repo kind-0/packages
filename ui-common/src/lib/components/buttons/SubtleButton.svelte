0c0cdbf7c7fe959bd33cb9d52c054cf69c48aea4
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2023-09-25 22:00:26 -0600

    add props to SubtleButton

--- ui-common/src/lib/components/buttons/SubtleButton.svelte
+++ ui-common/src/lib/components/buttons/SubtleButton.svelte
@@ -1,6 +1,8 @@
 <button class="
     btn h-11 bg-base-300 text-xs font-medium normal-case hover:bg-opacity-30 hover:border hover:border-zinc-400 {$$props.class??""}
-"  on:click>
+"  on:click
+    {...$$props}
+>
     <slot />
 </button>
 
