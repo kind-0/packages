1c47c71d00a16220a5a95b8e88f9189539612a41
Osvaldo Rosales <osvaldo.rosales.96@gmail.com>
2023-09-26 01:13:37 -0600

    fix SubtleButton with formaction

--- ui-common/src/lib/components/buttons/SubtleButton.svelte
+++ ui-common/src/lib/components/buttons/SubtleButton.svelte
@@ -1,7 +1,7 @@
-<button class="
-    btn h-11 bg-base-300 text-xs font-medium normal-case hover:bg-opacity-30 hover:border hover:border-zinc-400 {$$props.class??""}
-"  on:click
-    {...$$props}
+<button 
+    class="btn h-11 bg-base-300 text-xs font-medium normal-case hover:bg-opacity-30 hover:border hover:border-zinc-400 {$$props.class??""}"  
+    on:click
+    formaction={$$props.formaction}
 >
     <slot />
 </button>
