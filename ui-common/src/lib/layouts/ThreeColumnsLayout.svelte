1d5b7ccc80db35fa562fc2833f36513d95be9fa4
triesap <137732411+triesap@users.noreply.github.com>
2023-10-04 20:20:07 +0000

    small changes, ThreeColumnsLayout

--- ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
+++ ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
     import Toaster from "../components/Toaster/Toaster.svelte";
     import { rightSidebar } from "../stores/layout.js";
+
+    /*
+
+
+    */
 </script>
 
-<div class="w-screen lg:w-fit mx-0 lg:mx-auto flex flex-col gap-0 lg:gap-8 min-h-screen">
+<div class="w-screen flex flex-col gap-0 lg:gap-8 min-h-screen">
     {#if $$slots.navbar}
-        <div class="flex flex-row gap-32 sticky top-0 z-50">
+        <div class="flex flex-row sticky top-0 z-50">
             <slot name="navbar" />
         </div>
     {/if}
 
-    <div class="flex flex-row px-0 lg:px-4 gap-32 lg:mx-auto">
-        <div class="hidden lg:block w-auto lg:w-sidebar sticky max-h-[90vh] overflow-y-auto top-0">
+    <div class="flex flex-row w-full px-0 lg:px-2">
+        <div class="max-lg:hidden min-w-sidebar sticky top-0 max-h-[90vh] overflow-y-auto">
             {#if $$slots.sidebar}
                 <slot name="sidebar" />
             {/if}
         </div>
-        <div class="w-screen lg:w-main flex flex-col justify-start lg:justify-stretch">
+        <div class="flex flex-col basis-3/5 justify-start">
             <slot />
         </div>
-        <div class="hidden lg:block w-auto lg:w-sidebar sticky top-0 max-h-[90vh] overflow-y-auto">
+        <div class="max-lg:hidden basis-1/5 sticky top-0 max-h-[90vh] overflow-y-auto">
             {#if $$slots.rightSidebar}
                 <slot name="rightSidebar" />
             {:else if $rightSidebar?.component}
