60f470cbe45135754f16b406b1a1a32a1e24b032
Pablo Fernandez <p@f7z.io>
2023-10-04 14:43:28 +0300

    make menuitem more flexible

--- ui-common/src/lib/components/Menu/MenuItem.svelte
+++ ui-common/src/lib/components/Menu/MenuItem.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
     export let href: string = '#';
     export let active: boolean = false;
+    export let color: string = 'accent2';
 </script>
 
 <li class={$$props.class || ``}>
     <a
         {href}
-        class="flex flex-row flex-grow"
+        class="
+            flex flex-row flex-grow
+            {active ? `border-l-${color}` : ""}
+        "
         class:active={active}
         on:click
     >
@@ -20,7 +24,6 @@
     li a.active {
         @apply bg-transparent;
         @apply border-l-4 rounded-l-none -ml-2 pl-5;
-        @apply border-l-accent2;
         @apply text-base-100-content;
     }
 </style>
