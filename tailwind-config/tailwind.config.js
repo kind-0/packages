7434b874b3d7e1e63a9f12bdab2123649a709848
triesap <137732411+triesap@users.noreply.github.com>
2023-10-06 16:26:23 +0000

    modify ThreeColumnsLayout

--- tailwind-config/tailwind.config.js
+++ tailwind-config/tailwind.config.js
@@ -1,4 +1,4 @@
-const colors = require("tailwindcss/colors");
+//const colors = require("tailwindcss/colors");
 
 module.exports = {
   content: [
@@ -22,6 +22,9 @@ module.exports = {
       width: {
         'sidebar': '244px',
         'main': '700px',
+      },
+      maxWidth: {
+        'mainContentLg': '820px'
       },
       spacing: {
         '32': '32px'
--- ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
+++ ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
@@ -5,23 +5,36 @@
     //export let mainClass = ``
 </script>
 
+<<<<<<< Updated upstream
 <div class="flex flex-col px-0 lg:px-4 gap-0 lg:gap-8 min-h-screen">
+=======
+<div class="w-screen flex flex-col px-0 gap-0 min-h-screen">
+>>>>>>> Stashed changes
     {#if $$slots.navbar}
         <div class="flex flex-row sticky top-0 z-50">
             <slot name="navbar" />
         </div>
     {/if}
 
-    <div class="flex flex-row px-0 gap-32 lg:mx-auto">
-        <div class="hidden lg:block w-auto lg:w-sidebar sticky max-h-[90vh] overflow-y-auto top-0">
-            {#if $$slots.sidebar}
-                <slot name="sidebar" />
-            {/if}
+    <div class="flex flex-row gap-0 lg:gap-4 justify-between">
+        <div class="flex flex-row w-full justify-start gap-4">
+            <div class="max-lg:hidden flex flex-col w-sidebar max-h-[90vh] overflow-y-auto pl-2">
+                {#if $$slots.sidebar}
+                    <slot name="sidebar" />
+                {/if}
+            </div>
+            <div class="flex flex-col w-full lg:w-auto lg:flex-1">
+                <slot />
+            </div>
         </div>
+<<<<<<< Updated upstream
         <div class="flex flex-col basis-3/5 justify-start lg:w-main">
             <slot />
         </div>
         <div class="max-lg:hidden basis-1/5 sticky top-0 max-h-[90vh] overflow-y-auto lg:w-sidebar">
+=======
+        <div class="max-lg:hidden flex flex-col w-sidebar sticky top-0 max-h-[90vh] overflow-y-auto">
+>>>>>>> Stashed changes
             {#if $$slots.rightSidebar}
                 <slot name="rightSidebar" />
             {:else if $rightSidebar?.component}
