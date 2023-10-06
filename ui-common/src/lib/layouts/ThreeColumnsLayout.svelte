7461beea78fba86bbca26920b7814f0cc2a97f2c
triesap <137732411+triesap@users.noreply.github.com>
2023-10-06 16:34:03 +0000

    modify ThreeColumnsLayout

--- ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
+++ ui-common/src/lib/layouts/ThreeColumnsLayout.svelte
@@ -1,15 +1,9 @@
 <script lang="ts">
     import Toaster from "../components/Toaster/Toaster.svelte";
     import { rightSidebar } from "../stores/layout.js";
-
-    //export let mainClass = ``
 </script>
 
-<<<<<<< Updated upstream
-<div class="flex flex-col px-0 lg:px-4 gap-0 lg:gap-8 min-h-screen">
-=======
 <div class="w-screen flex flex-col px-0 gap-0 min-h-screen">
->>>>>>> Stashed changes
     {#if $$slots.navbar}
         <div class="flex flex-row sticky top-0 z-50">
             <slot name="navbar" />
@@ -27,14 +21,7 @@
                 <slot />
             </div>
         </div>
-<<<<<<< Updated upstream
-        <div class="flex flex-col basis-3/5 justify-start lg:w-main">
-            <slot />
-        </div>
-        <div class="max-lg:hidden basis-1/5 sticky top-0 max-h-[90vh] overflow-y-auto lg:w-sidebar">
-=======
         <div class="max-lg:hidden flex flex-col w-sidebar sticky top-0 max-h-[90vh] overflow-y-auto">
->>>>>>> Stashed changes
             {#if $$slots.rightSidebar}
                 <slot name="rightSidebar" />
             {:else if $rightSidebar?.component}
